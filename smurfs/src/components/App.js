56100524b6e124fe34839f4afd5ddea1895e8a5c
rae-glazier <glazier.home.life@gmail.com>
2019-12-13 13:12:17 -0600

    bleh

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,24 +1,16 @@
 import React from "react";
 import "./App.css";
-import { Provider } from 'react-redux';
+// import { Provider } from 'react-redux';
 
-import Smurfs from './smurfs';
-import SmurForm from './smurfForm';
-import store from '../store';
 
 
 function App () {
 
     return (
-      <Provider store={store}>
         <div className="App">
           <h1>SMURFS! 2.0 W/ Redux</h1>
 
-          <SmurForm />
-          <Smurfs />
-
         </div>
-      </Provider>
     );
   
 }
