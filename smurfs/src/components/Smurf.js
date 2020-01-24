f670032abf92659253698cea3816f4b044e33f66
rae-glazier <glazier.home.life@gmail.com>
2020-01-24 10:54:52 -0600

    working on stretch goals web26

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -4,6 +4,7 @@ const Smurf = props => {
     return(
         <div>
             <h3>Name: {props.smurf.name}</h3>
+            <h4>ID: {props.smurf.id}</h4>
             <h4>Age: {props.smurf.age}</h4>
             <h4>Height: {props.smurf.height}</h4>
         </div>
