49989805fc3432fcbc3761af8619c1531b88584f
rae-glazier <glazier.home.life@gmail.com>
2020-01-24 11:54:02 -0600

    still working on adding editing & deleting capabilities

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,12 +1,68 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import axios from 'axios';
+import { SmurfContext } from './SmurfContext';
 
-const Smurf = props => {
+
+const initialSmurf = {
+    smurf: '',
+    id: { id: '' }
+};
+
+const Smurf = (smurfs, updateSmurfs) => {
+    const sInfo = useContext(SmurfContext);
+    console.log(smurfs);
+    const [smurfEdit, setSmurfEdit] = useState(initialSmurf);
+
+    
+
+    const updateSmurfList = () => {
+        axios
+            .get('http://localhost:3333/smurfs')
+            .then(response => {
+                setSmurfEdit(response.data);
+            })
+            .catch(error => console.log(error.response, 'updateSmurfList'));
+    };
+
+    const sEdit = e => {
+        // e.preventDefualt();
+
+        axios 
+            .put(`http://localhost:3333/smurfs/${smurfs.smurf.id}`, smurfEdit)
+            .then(updateSmurfList())
+            .catch(error => console.log(error.response, 'edit smurf'))
+    }
+
+    const deleteSmurf = () => {
+        axios   
+            .delete(`http://localhost:3333/smurfs/${smurfs.smurf.id}`)
+            .then(updateSmurfList())
+            .catch(error => console.log(error, 'delete'))
+    }
+
+
     return(
-        <div>
-            <h3>Name: {props.smurf.name}</h3>
-            <h4>ID: {props.smurf.id}</h4>
-            <h4>Age: {props.smurf.age}</h4>
-            <h4>Height: {props.smurf.height}</h4>
+        <div className='SmurfInfo'>
+
+            <ul>
+                {sInfo.map(smurf => {
+                    <li key={smurf.id} onClick= {() => sEdit(smurfs)}>
+                        <span>
+                            <span className = 'delete' onClick={e => {e.stopPropagation(); deleteSmurf(smurf)}}>
+                                ~delete~
+                            </span>{''} 
+                            {smurf.smurf}
+                        </span>
+                        <div>
+                            <h3>Name: {smurfs.smurf.name}</h3>
+                            <h4>ID: {smurfs.smurf.id}</h4>
+                            <h4>Age: {smurfs.smurf.age}</h4>
+                            <h4>Height: {smurfs.smurf.height}</h4>
+                        </div>
+                    </li>
+                })}
+            </ul>
+            
         </div>
     )
 }
--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -1,53 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { SmurfContext } from './SmurfContext';
 import  Smurf  from './Smurf';
 
-const initialSmurf = {
-    smurf: '',
-    id: { id: '' }
-};
+
+
 
 const SmurfList = () => {
-    const sInfo = useContext(SmurfContext);
+    // const sInfo = useContext(SmurfContext);
     
-    const [smurfEdit, setSmurfEdit] = useState(initialSmurf);
-
-    const editSmurf = smurf => {
-        setSmurfEdit(smurf);
-    };
-
-    const updateSmurfList = () => {
-        axios
-            .get('http://localhost:3333/smurfs')
-            .then(response => {
-                updateSmurfList(response.data);
-            });
-    };
-
-    const smurfEdit = e => {
-        e.preventDefualt();
-
-        axios 
-            .put(`http://localhost:3333/smurfs${smurfEdit.id}`, smurfEdit)
-            .then(updateSmurfList())
-            .catch(error => console.log(error.response, 'edit smurf'))
-    }
-
-    const deleteSmurf = smurf => {
-        axios   
-            .get(`http://localhost:3333/smurfs${smurf.id}`)
-            .then(updateSmurfList())
-            .catch(error => console.log(error, 'delete'))
-    }
+    
 
     return(
         <div>
-            {sInfo.map(smurf => {
-                return <Smurf 
-                            key={smurf.id} 
-                            smurf={smurf}
-                       />
-            })}
+            <h1>Smurf Village</h1>
+            {/* <ul>
+                {sInfo.map(smurf => {
+                    return <Smurf 
+                                key={smurf.id}  
+                                smurf={smurf} 
+                                />
+                })}
+            </ul> */}
+
+            <Smurf />
+            
         </div>
     )
 }
