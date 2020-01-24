ef9715d3820e46c43c3760d79d3e49b15c620639
rae-glazier <glazier.home.life@gmail.com>
2020-01-24 12:46:10 -0600

    couldnt get the edit to work .. sticking with delete

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -25,13 +25,18 @@ const Smurf = (smurfs, updateSmurfs) => {
             .catch(error => console.log(error.response, 'updateSmurfList'));
     };
 
-    const sEdit = smurf => {
+    const sEdit = (smurf) => {
         // e.preventDefualt();
 
         axios 
-            .put(`http://localhost:3333/smurfs/${smurf.id}`, smurfEdit)
-            .then(updateSmurfList())
+            .put(`http://localhost:3333/smurfs/${smurf.id}`)
+            .then(response => {
+                console.log(response.data)
+                updateSmurfList(response.data)
+            })
             .catch(error => console.log(error.response, 'edit smurf'))
+
+            
     }
 
     const deleteSmurf = (smurf) => {
@@ -51,70 +56,33 @@ const Smurf = (smurfs, updateSmurfs) => {
             <div>
                 {sInfo.map(smurf => {
                     return(
-                        <div key={smurf.id} onClick= {() => sEdit(smurfs)}>
-                        <span>
-                            <span className = 'delete' onClick={e => {e.stopPropagation(); deleteSmurf(smurf)}}>
-                                x
-                            </span>{''} 
-                            {smurf.smurf}
-                        </span>
+                        <div key={smurf.id} onClick= {() => sEdit(smurf)}>
+                        
                         <div>
                             <h3>Name: {smurf.name} </h3>
                             <h4>ID: {smurf.id} </h4>
                             <h4>Age: {smurf.age} </h4>
                             <h4>Height: {smurf.height} </h4>
                         </div>
+
+                        <span>
+                            <span className = 'delete' onClick={e => {e.stopPropagation(); deleteSmurf(smurf)}}>
+                                delete
+                            </span>{''} 
+                            {smurf.smurf}
+                        </span>
+                        {/* <br></br>
+                        <span>
+                            <span className = 'edit' onClick={e => {e.stopPropagation(); sEdit(smurf);}}>
+                                edit
+                            </span>{''} 
+                            {smurf.smurf}
+                        </span> */}
+
                     </div>
                     )
                 })}
             </div>
-            
-
-        {editing && (
-            <form onSubmit={sEdit}>
-                <legend>edit smurf</legend>
-                <label>
-                Name:
-                <input
-                    onChange={e =>
-                    setSmurfEdit({ ...smurfEdit, Name: e.target.value })
-                    }
-                    value={smurfEdit.smurf.name}
-                />
-                </label>
-
-                <label>
-                Age:
-                <input
-                    onChange={e =>
-                        setSmurfEdit({
-                        ...smurfEdit,
-                        Age: e.target.value 
-                    })
-                    }
-                    value={smurfEdit.smurf.age}
-                />
-                </label>
-
-                <label>
-                Height:
-                <input
-                    onChange={e =>
-                        setSmurfEdit({
-                        ...smurfEdit,
-                        Height: e.target.value 
-                    })
-                    }
-                    value={smurfEdit.smurf.height}
-                />
-                </label>
-
-                <div className="button-row">
-                <button type="submit">save</button>
-                <button onClick={() => setEditing(false)}>cancel</button>
-                </div>
-            </form>
-        )}
 
         </div>
     )
