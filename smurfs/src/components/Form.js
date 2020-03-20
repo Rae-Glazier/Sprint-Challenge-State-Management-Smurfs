7073c0232665bd11e0f9cba79d069e9cfee10a85
rae-glazier <glazier.home.life@gmail.com>
2020-03-20 12:35:15 -0500

    cleaning up code

--- smurfs/src/components/Form.js
+++ smurfs/src/components/Form.js
@@ -8,62 +8,52 @@ function Form () {
         height: ''
     })
     
+    const onSubmit = () => {
+        axios.post('http://localhost:3333/smurfs', form)
+            .then(res => {
+                console.log(res)
+                setForm(form = [...form, res])
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    const onChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        })
+    }
 
     return(
         <div className='form'>
-            <form onSubmit= {e => {
-
-// e.preventDefault();
-
-axios.post('http://localhost:3333/smurfs', form)
-    .then(res => {
-        console.log(res)
-        setForm(form = [...form, res])
-    })
-    .catch(err => {
-        console.log(err)
-    })
-}}>
-<input 
-    name='name'
-    placeholder= 'Name'
-    value = {form.name}
-    onChange={ e => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
-        })
-    }}
-/>
-<br />
-<input 
-    name='age'
-    placeholder= 'Age'
-    value = {form.age}
-    onChange={ e => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
-        })
-    }}
-/>
-<br />
-<input 
-    name='height'
-    placeholder= 'Height'
-    value = {form.height}
-    onChange={ e => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
-        })
-    }}
-/>
-<br />
-<button type='submit'>Smurf It Up!!</button>
-</form>
+            <form onSubmit={onSubmit}>
+                <input 
+                    name='name'
+                    placeholder= 'Name'
+                    value = {form.name}
+                    onChange={onChange}
+                />
+                <br />
+                <input 
+                    name='age'
+                    placeholder= 'Age'
+                    value = {form.age}
+                    onChange={onChange}
+                />
+                <br />
+                <input 
+                    name='height'
+                    placeholder= 'Height'
+                    value = {form.height}
+                    onChange={onChange}
+                />
+                <br />
+                <button type='submit'>Smurf It Up!!</button>
+            </form>
         </div>
-        
+                        
     )
 }
 
