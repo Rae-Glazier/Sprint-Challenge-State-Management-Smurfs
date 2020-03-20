bbeed55bce89b5b86d1d96e744f5b4f08f8c91ec
rae-glazier <glazier.home.life@gmail.com>
2020-03-20 11:12:18 -0500

    actions built out more

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,2 +1,29 @@
 export const FETCH_SMURF = 'FETCH_SMURF';
 export const POST_SMURF = 'POST_SMURF';
+
+export const fetchSmurf = () => {
+    fetch('http://localhost:3333/smurfs')
+        .then (res => res.json())
+        .then(smurfs => 
+            dispatchEvent({
+                type: FETCH_SMURF,
+                payload: smurfs
+            }))
+}
+
+export const postSmurf = smurfData => dispatch => {
+    fetch('http://localhost:3333/smurfs', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(smurfData)
+    })
+    .then(res => res.json())
+    .then(smurf => 
+        dispatch({
+            type: POST_SMURF,
+            payload: smurf
+        })
+        )
+}
